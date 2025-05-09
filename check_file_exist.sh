2192987fcbd6dc6bf807dd2baa61e746c14e5529
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-09 22:45:18 +0100

    Add files via upload

---
+++ check_file_exist.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+#demander a l utulisateur d entrer le nom du fichier
+read -p  "Entrez le nom du fichier a verifier :"
+if [-e"€sorkho"];then
+echo "le fichier existe."
+else
+echo "le fichier n'existe pas."
+fi
