519e695b557102e1bffe14f13cedaa5814a9c90a
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-09 23:03:45 +0100

    Delete check_file_exist.sh

--- check_file_exist.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-#demander a l utulisateur d entrer le nom du fichier
-read -p  "Entrez le nom du fichier a verifier :"
-if [-e"€sorkho"];then
-echo "le fichier existe."
-else
-echo "le fichier n'existe pas."
-fi
