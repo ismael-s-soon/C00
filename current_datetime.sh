0b6a927621bd6067337ee247481d31d67a3fbaeb
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-09 21:44:06 +0100

    Delete current_datetime.sh

--- current_datetime.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# la variable date_actuelle qui contien le script qui cree la date actuelle
-Date_actuelle=$(date +"%y-%m-%d %H:%M:%S")
-#la variable Heure_actuelle qui contient le srcipt de l'heure actuele
-echo  "$Date_actuelle"
-
