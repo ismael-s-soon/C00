9913db7495646db8572e844a641898808c769f3a
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-10 12:37:51 +0100

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Demande à l'utilisateur le nom du fichier
-echo "Entrez le nom du fichier:" 
+echo "Entrez le nom du fichier :" 
 read filename
 # Vérifie si le fichier existe et est un fichier ordinaire
 if [ -f "$filename" ]; then
