a9faf421b815504814aa012023162e19d09270a0
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-14 21:04:50 +0100

    Add files via upload

--- count_files.sh
+++ count_files.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-# lire le nom du dossier que l'utilisateur va saisir
-read dossier
-# Vérifie si le dossier existe
-if [ ! -d "$dossier" ]; then
-  echo "Le dossier '$dossier' n'existe pas."
-  exit 1
+
+# Nom du dossier
+dossier="myfloder"
+
+# Vérifier si le dossier existe
+if [ -d "$dossier" ]; then
+    nb_elements=$(ls "$dossier" | wc -l)
+echo "$dossier"
+    echo "Le dossier $dossier contient $nb_elements fichier(s)."
+else
+    echo "Le dossier $dossier n'existe pas."
 fi
-# Se place dans le dossier
-cd "$dossier"
-# Initialisation du compteur
-nb_fichiers=0
-# Boucle sur chaque élément listé par ls
-for element in $(ls); do
-  if [ -f "$element" ]; then
-    nb_fichiers=$((nb_fichiers + 1))
-  fi
-done
-# Affichage du résultat
-echo "Le dossier $dossier contient $nb_fichiers fichier(s)."
