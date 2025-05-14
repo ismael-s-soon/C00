36557d43bc00bd1f030faf7f4fd3e51fe9b46c6f
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-14 21:13:23 +0100

    Add files via upload

--- count_files.sh
+++ count_files.sh
@@ -4,10 +4,6 @@
 dossier="myfloder"
 
 # Vérifier si le dossier existe
-if [ -d "$dossier" ]; then
     nb_elements=$(ls "$dossier" | wc -l)
 echo "$dossier"
     echo "Le dossier $dossier contient $nb_elements fichier(s)."
-else
-    echo "Le dossier $dossier n'existe pas."
-fi
