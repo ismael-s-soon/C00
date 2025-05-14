e1858c436522fa93a0031b9673adfba9c675a719
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-14 13:18:11 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
-
 # lire le nom du dossier que l'utilisateur va saisir
 read dossier
-
 # Vérifie si le dossier existe
 if [ ! -d "$dossier" ]; then
   echo "Le dossier '$dossier' n'existe pas."
   exit 1
 fi
-
 # Se place dans le dossier
 cd "$dossier"
-
 # Initialisation du compteur
 nb_fichiers=0
-
 # Boucle sur chaque élément listé par ls
 for element in $(ls); do
   if [ -f "$element" ]; then
     nb_fichiers=$((nb_fichiers + 1))
   fi
 done
-
 # Affichage du résultat
 echo "Le dossier $dossier contient $nb_fichiers fichier(s)."
