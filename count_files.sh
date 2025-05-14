65ddb2941ff1ac88d9a88e8cda5b576bb97acc8c
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-14 21:36:16 +0100

    Add files via upload

--- count_files.sh
+++ count_files.sh
@@ -2,12 +2,8 @@
 
 dossier="myfloder"
 compteur=0
-
-for element in $(ls "$dossier" 2>/dev/null); do
-  if [ -f "$dossier/$element" ]; then
-    compteur=$((compteur + 1))
-  fi
-done
+nb_fichiers=$(ls "$dossier" 2>/dev/null | wc -l)
+compteur=$((compteur + 1))
 echo "$dossier"
 echo ""
 echo "Le dossier $dossier contient $compteur fichier(s)."
