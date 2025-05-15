b2648e7eaeb471b77a360a6a316c7e189edf0430
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-15 23:37:44 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-#creation du repertoire dans lequel nous alllons compter le nombre de fichier
-mkdir -p myfolder
 #nous allons essayer de compter les nombres de fichiers dans un repertoir
 repertoire="myfolder"
 compteur=0
