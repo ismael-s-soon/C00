918ac82fe4463041cb3b31330611ff57681051e4
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-15 22:37:56 +0100

    Add files via upload

--- count_files.sh
+++ count_files.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-#compter le nombre de fichiers dans un répertoire 
+#creation du repertoire dans lequel nous alllons compter le nombre de fichier
+mkdir -p myfolder
+#nous allons essayer de compter les nombres de fichiers dans un repertoir
+repertoire="myfolder"
+compteur=0
+nb_fichiers=$(ls "$repertoire" | wc -l)
+compteurs=$((compteur ++))
+echo "$repertoire"
+echo ""
+echo "Le dossier $repertoire contient $nb_fichiers fichier(s)."
