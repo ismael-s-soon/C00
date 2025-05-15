72cb99d37881c54baca5b729cb3326452753c17f
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-15 20:44:10 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
 #compter le nombre de fichiers dans un répertoire 
-dossier="myfolder"
-compteur=0
-nb_fichiers=$(ls "$dossier" 2>/dev/null | wc -l)
-compteur=$((compteur + 1))
-echo "$dossier"
-echo ""
-echo "Le dossier $dossier contient $compteur fichier(s)."
