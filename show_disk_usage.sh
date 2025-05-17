1a7cbcf0e10a6e496baa5c070a76dd79ff89b248
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-17 17:16:04 +0100

    Delete show_disk_usage.sh

--- show_disk_usage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#nous allons creer ce script qui permet de afficher l'utilisation du disque  de façon clair
-df -h
-# cette commande affiche l'espace disque disponible
-du -sh ./* 2>/dev/null
