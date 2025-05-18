ad2af828ecbd3060175824e9667fddf1b2a310bd
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-18 03:07:07 +0100

    Update find_string.sh

--- find_string.sh
+++ find_string.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#nous allons créer d'abord le fichier texte 
-mkdir sample.txt
+
 #nous allons creer deux variables
 read f
 #celle la contient le fichier 
@@ -8,7 +7,8 @@ read c
 #et cellle la contient la chaine 
 #nous allons maintenant proceder a la recherche de la chaine dans le fichier a l'aide de grep
 if grep -q "$c" "$f"; then 
-echo "La chaine '$c' a été trouvée dans $f."
+echo "La chaîne '$c' a été trouvée dans $f."
 else
-echo "La chaine '$c' n'a pas été trouvée dans $f."
+echo "La chaîne '$c' n'a pas été trouvée dans $f."
 fi
+exit 0
